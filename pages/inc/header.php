1dd3365d528902660c6b5927d9b512b9db323622
Kristian Válek <valekkristian2@gmail.com>
2023-04-04 11:30:13 +0200

    first commit

---
+++ pages/inc/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">AVIO-TRAVEL</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Domov</a>
+              <a class="nav-link" href="#">Destinacie</a>
+              <a class="nav-link" href="#">O nas</a>
+              <a class="nav-link disabled">Kontakt</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+    
---
+++ pages/index.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'inc/header.php';
+
+?>
+    
+    <h1>Hello, world!</h1>
+
+
+
+<?php
+include 'inc/footer.php';
+
+?>
